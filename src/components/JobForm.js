caaf56f3134ffe2a990fa8a73b2c5358db0972e9
mstafford93 <mgstafford@hotmail.co.uk>
2026-08-25 16:23:14 +0100

    list items with css basic

--- src/components/JobForm.js
+++ src/components/JobForm.js
@@ -1,6 +1,28 @@
-import React from 'react';
+import React, { useState } from 'react';
+import JobItem from './JobItem';
 const JobForm = () => {
+    const [listItems, setListItems] = useState([])
+    const [addItem, SetAddItem] = useState({ job: "", status: "" })
+
+
+    const handleInputChangeJob = (event) => {
+        SetAddItem({ ...addItem, job: event.target.value })
+    }
+
+    const handleInputChangeStatus = (event) => {
+        SetAddItem({ ...addItem, status: event.target.value })
+    }
+
+    const addItemToList = (event) => {
+        setListItems([...listItems, addItem])
+        SetAddItem({ job: "", status: "" })
+        console.log(listItems)
+        event.preventDefault()
+    }
+
+
     return (
+        <>
         <div className="form-header">
             <form>
                 <div className='botInput'>
@@ -8,16 +30,17 @@ const JobForm = () => {
                         type="text"
                         className="bot-input"
                         placeholder="Enter the job"
+                        onChange={handleInputChangeJob}
                     />
                     <div className='select'>
-                    <select className="job-status">
-                        <option value="start">Start Process</option>
-                        <option value="running">Running</option>
-                        <option value="completed">Completed</option>
-                        <option value="stopped">Stopped</option>
-                    </select>
-                </div>
-                    <button type="submit" className="submit-data">Add Job</button>
+                        <select className="job-status" onChange={handleInputChangeStatus}>
+                            <option value="start">Start Process</option>
+                            <option value="running">Running</option>
+                            <option value="completed">Completed</option>
+                            <option value="stopped">Stopped</option>
+                        </select>
+                    </div>
+                    <button type="submit" className="submit-data" onClick={addItemToList}>Add Job</button>
                 </div>
                 <div className="form-details">
                     <div className="bottom-line">
@@ -28,6 +51,12 @@ const JobForm = () => {
                 </div>
             </form>
         </div>
+        <div>
+                <ul>
+                    {listItems.map((task, index) => <JobItem key={index} task={task} />)}
+                </ul>
+            </div>
+        </>
     );
 };
 
---
+++ src/components/JobItem.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const JobItem = ({task}) => {
+  return (
+                <li className='li-item'><span>Job Name: {task.job}</span><span>Status: {task.status}</span></li>
+  )
+}
+
+export default JobItem
