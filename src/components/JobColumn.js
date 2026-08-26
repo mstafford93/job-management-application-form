1551fbcfb726ca78ccd0167328a517c13af0a209
mstafford93 <mgstafford@hotmail.co.uk>
2026-08-26 10:34:53 +0100

    change select and alreat parameters

--- src/components/JobColumn.js
+++ src/components/JobColumn.js
@@ -1,11 +1,12 @@
 import React from 'react'
 import './JobColumn.css'
 
-const JobColumn = ({value, image}) => {
+const JobColumn = ({value, image, children}) => {
   return (
     <section className='singleSection'>
         <h2>{value}</h2>
         <img src={image} alt="Task Icon" className='sectionImage'/>
+        {children}
     </section>
   )
 }
--- src/components/JobForm.js
+++ src/components/JobForm.js
@@ -1,16 +1,23 @@
 import React, { useState } from 'react';
-import JobItem from './JobItem';
+import JobColumn from './JobColumn';
+import Completed from '../images/completed.png';
+import InProgress from '../images/in-progress.png';
+import Failed from '../images/failed.png'
 import "./AppForm.css"
+import JobItem from './JobItem';
 
 const JobForm = () => {
     const [listItems, setListItems] = useState([])
     const [addItem, SetAddItem] = useState({ job: "", status: "" })
     const [toggleSwitch, setToggleSwitch] = useState(false)
+    const [needToStart, setNeedToStart] = useState([])
+    const [inProgress, setInProgress] = useState([])
+    const [completed, setCompleted] = useState([])
 
     const handleToggleMode = () => {
         let body = document.body.classList
         setToggleSwitch(!toggleSwitch)
-        if (toggleSwitch === true){
+        if (toggleSwitch === true) {
             body.add("lightMode")
         } else {
             body.remove("lightMode")
@@ -28,52 +35,53 @@ const JobForm = () => {
 
     const addItemToList = (event) => {
         event.preventDefault()
-        if(addItem.job === "") {
-            alert("Please enter job")
-        } else {
-        setListItems([...listItems, addItem])
-        SetAddItem({ job: "", status: "" })
-        //console.log(listItems)
+        if(addItem.status === "" || addItem.job === ""){
+            return alert ("Please Enter Job and Status")
         }
-    }
+            setListItems([...listItems, addItem])
+            SetAddItem({ job: "", status: "" })
+            //console.log(listItems)
+        }
 
 
     return (
         <>
-        <button className='darkmode-toggle' onClick={handleToggleMode}>Dark Mode Toggle</button>
-        <div className="form-header">
-            <form>
-                <div className='botInput'>
-                    <input
-                        type="text"
-                        className="bot-input"
-                        placeholder="Enter the job"
-                        onChange={handleInputChangeJob}
-                        value={addItem.job}
-                    />
-                    <div className='select'>
-                        <select className="job-status" onChange={handleInputChangeStatus} value={addItem.status}>
-                            <option value="Start">Start Process</option>
-                            <option value="Running">Running</option>
-                            <option value="Completed">Completed</option>
-                            <option value="Stopped">Stopped</option>
-                        </select>
+            <button className='darkmode-toggle' onClick={handleToggleMode}>Dark Mode Toggle</button>
+            <div className="form-header">
+                <form>
+                    <div className='botInput'>
+                        <input
+                            type="text"
+                            className="bot-input"
+                            placeholder="Enter the job"
+                            onChange={handleInputChangeJob}
+                            value={addItem.job}
+                        />
+                        <div className='select'>
+                            <select className="job-status" onChange={handleInputChangeStatus} value={addItem.status}>
+                                <option value="" disabled>Status</option>
+                                <option value="Need To Start">Need To Start</option>
+                                <option value="In-Progress">In-Progress</option>
+                                <option value="Completed">Completed</option>
+                            </select>
+                        </div>
+                        <button type="submit" className="submit-data" onClick={addItemToList}>Add Job</button>
                     </div>
-                    <button type="submit" className="submit-data" onClick={addItemToList}>Add Job</button>
-                </div>
-                <div className="form-details">
-                    <div className="bottom-line">
-                        <button>Read Emails</button>
-                        <button>Web Parsing</button>
-                        <button>Send Emails</button>
+                    <div className="form-details">
+                        <div className="bottom-line">
+                            <button>Read Emails</button>
+                            <button>Web Parsing</button>
+                            <button>Send Emails</button>
+                        </div>
                     </div>
-                </div>
-            </form>
-        </div>
-        <div>
-                <ul>
-                    {listItems.map((task, index) => <JobItem key={index} task={task} />)}
-                </ul>
+                </form>
+            </div>
+            <div className='columnSection'>
+                <JobColumn value="Need to Start" image={Failed}> 
+                    {listItems.map((task, index) => <JobItem key={index} task={task}/>)}
+                </JobColumn>
+                <JobColumn value="In Progress" image={InProgress} />
+                <JobColumn value="Completed" image={Completed} />
             </div>
         </>
     );
